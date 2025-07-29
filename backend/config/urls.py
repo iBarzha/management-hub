b24d497e7e6edf0da8559823ca2ff4bae3c31f5b
iBarzha <grytcaie@gmail.com>
2025-07-29 21:11:26 +0300

    github migrations

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('api/', include('projects.urls')),
     path('api/', include('tasks.urls')),
     path('api/collaboration/', include('collaboration.urls')),
+    path('api/integrations/', include('integrations.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
