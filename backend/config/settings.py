56eca232a555fa4ae3b7583ca75bc5ab01671398
iBarzha <grytcaie@gmail.com>
2025-08-30 17:23:29 +0300

    update settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -136,8 +136,7 @@ SIMPLE_JWT = {
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    "https://barzha-management-hub.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = DEBUG
